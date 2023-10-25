2d133bca9d75c121e917166c21ab2c992cbb97ab
adstraw <astraw@octoml.ai>
2023-10-25 17:13:08 +0000

    fix fp16 quality issue; use CodeLlama example

--- scripts/build-llama-fp16.sh
+++ scripts/build-llama-fp16.sh
@@ -6,4 +6,17 @@ if [[ $# -ne 2 ]] ; then
     exit 1
 fi
 
-python $TRTLLM_HOME/examples/llama/build.py --model_dir $1 --dtype float16 --use_gpt_attention_plugin float16 --use_gemm_plugin float16 --output_dir $2 --max_input_len 2364 --max_output_len 2364 --max_batch_size 1
+python $TRTLLM_HOME/examples/llama/build.py \
+    --model_dir $1 \
+    --dtype float16 \
+    --remove_input_padding \
+    --use_gpt_attention_plugin float16 \
+    --use_gemm_plugin float16 \
+    --use_rmsnorm_plugin float16 \
+    --enable_context_fmha \
+    --output_dir $2 \
+    --rotary_base 1000000 \
+    --vocab_size 32000 \
+    --max_input_len 2364 \
+    --max_output_len 2364 \
+    --max_batch_size 1
